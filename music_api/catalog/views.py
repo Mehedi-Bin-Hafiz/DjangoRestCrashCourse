1b2dec82214c2ddb92028720a97f197474793b9a
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2022-05-28 22:28:02 +0600

    first video is completed

--- music_api/catalog/views.py
+++ music_api/catalog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import  viewsets
+from rest_framework import  permissions
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """API Endpoint that allows user to be viewed or edited"""
+    queryset = User.objects.all().order_by('-date_joined') # - means newest first oldest last
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
