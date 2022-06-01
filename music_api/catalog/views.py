57379a4a1c4ac5f8e241c3c8c60012d7696f6121
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2022-06-01 12:00:05 +0600

    introduction to generic views

--- music_api/catalog/views.py
+++ music_api/catalog/views.py
@@ -1,11 +1,19 @@
 from django.contrib.auth.models import User
 from rest_framework import  viewsets
-from rest_framework import  permissions
-from .serializers import UserSerializer
-from rest_framework.decorators import api_view
+from .serializers import UserSerializer, ArtistSerializer, SongSerializer
+from rest_framework.decorators import api_view, throttle_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from . models import Artist
+from rest_framework import permissions
+from rest_framework import  authentication
+from rest_framework.generics import GenericAPIView
+
+from rest_framework.throttling import UserRateThrottle
+
+class OncePerDayUserThrottle(UserRateThrottle):
+   rate = '/day'
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """API Endpoint that allows user to be viewed or edited"""
@@ -22,10 +30,20 @@ def function_based(request):
         return  Response({"message":"I am active"})
 #### class based views
 
-class ArtistView(APIView):
-    def get(self, request):
-        artist = Artist.objects.all()
-        return Response(artist)
-    def post(self, request):
-        return Response({'message':request.data})
+# class ArtistView(APIView):
+#     authentication_classes = [authentication.TokenAuthentication]
+#     permission_classes = [permissions.IsAuthenticated]
+#     throttle_classes = [OncePerDayUserThrottle]
+#
+#     def get(self, request):
+#         artist = Artist.objects.all()
+#         return Response(artist)
+#     def post(self, request):
+#         return Response({'message':request.data})
 
+class ArtistGenericView(GenericAPIView):
+    queryset =  Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+    def get(self, request, *args, **kwargs):
+        return
